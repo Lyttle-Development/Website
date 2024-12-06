29485664cf78a59392a9a897a061b245e3138fee
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2024-12-06 14:46:54 +0100

    Add some styling

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -31,7 +31,7 @@ export async function Footer() {
             <img
               className={classes.logo}
               alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/payload/src/admin/assets/images/payload-logo-light.svg"
+              src="/logo.svg"
             />
           </picture>
         </Link>
@@ -40,17 +40,6 @@ export async function Footer() {
           {navItems.map(({ link }, i) => {
             return <CMSLink key={i} {...link} />
           })}
-          <Link href="/admin">Admin</Link>
-          <Link
-            href="https://github.com/payloadcms/payload/tree/main/templates/website"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source Code
-          </Link>
-          <Link href="https://payloadcms.com" target="_blank" rel="noopener noreferrer">
-            Payload
-          </Link>
         </nav>
       </Gutter>
     </footer>
