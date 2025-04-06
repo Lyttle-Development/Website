c4ac77eb281097a9f8cc36f87841f27fff5a8827
stualyttle <stualyttle@users.noreply.github.com>
2025-04-06 22:44:21 +0200

    Add loading fade

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { DomLoadedProvider } from '@/providers/DomLoaded'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
-  return <>{children}</>
+  return <DomLoadedProvider>{children}</DomLoadedProvider>
 }
