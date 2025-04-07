b2b8f76e90fcb4adce7b68d8f0dfce8bc0e8e009
stualyttle <stualyttle@users.noreply.github.com>
2025-04-07 16:54:03 +0200

    Fix Space Background banner

--- src/blocks/decorative/PlanetsVisual/Component.tsx
+++ src/blocks/decorative/PlanetsVisual/Component.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import styles from './Component.module.scss'
 import classNames from 'classnames'
-import { RandomStars } from '@/components/RandomStars'
 
 export interface PlanetsVisualProps {}
 
 export const PlanetsVisualBlock: React.FC<PlanetsVisualProps> = (props) => {
-  return (
-    <div className={classNames(styles.planetsVisual)}>
-      <img
-        src="/svgs/home/headerBackgroundBanner.svg"
-        alt="Header Background Banner"
-        className={styles.backgroundBanner}
-      />
-      <RandomStars width="100vw" height="calc(100vh - 2rem)" />
-    </div>
-  )
+  return <div className={classNames(styles.planetsVisual)}></div>
 }
--- src/components/Footer/Component.tsx
+++ src/components/Footer/Component.tsx
@@ -13,7 +13,12 @@ export async function Footer() {
   const navItems = footerData?.navItems || []
 
   return (
-    <footer className="mt-auto border-t border-border bg-black bg-card text-white">
+    <footer
+      className="mt-auto border-t border-border bg-black bg-card text-white"
+      style={{
+        marginTop: '250vh',
+      }}
+    >
       <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
         <Link className="flex items-center" href="/">
           <Logo />
