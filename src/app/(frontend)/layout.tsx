727249ec10bf12b49792419ec09bfec9efb281d7
stualyttle <stualyttle@users.noreply.github.com>
2025-04-13 15:56:20 +0200

    Use own umami

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -48,10 +48,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {/* Safari/WebKit Auto Dark Mode */}
         <meta name="color-scheme" content="light only" />
 
-        {/* Implement umami cloud */}
+        {/* Add umami */}
         <script
           defer
-          src="https://cloud.umami.is/script.js"
+          src="https://umami.app.lyttle.dev/script.js"
           data-website-id={process.env.UMAMI_WEBSITE_ID}
         />
         <noscript>
