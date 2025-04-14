6dd5c79dea9bb485158279cbf341cef657da2484
stualyttle <stualyttle@users.noreply.github.com>
2025-04-14 21:24:44 +0200

    Fix translation issues

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -84,7 +84,7 @@ export const plugins: Plugin[] = [
     },
   }),
   searchPlugin({
-    collections: ['posts'],
+    collections: ['posts', 'pages'],
     beforeSync: beforeSyncWithSearch,
     searchOverrides: {
       fields: ({ defaultFields }) => {
