96d6cd6b69c39af470fbb25645d4310f98ad96fe
Stualyttle Kirry <Stualyttle@users.noreply.github.com>
2025-04-16 15:34:00 +0200

    Add new plugins

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -20,6 +20,12 @@ import { getServerSideURL } from '@/utilities/getURL'
 import { uploadthingStorage } from '@payloadcms/storage-uploadthing'
 import { WEBSITE_NAME } from '../../constants'
 
+// Custom plugins:
+import {
+  betterLocalizedFields,
+} from '@payload-enchants/better-localized-fields'
+import { docsReorder } from '@payload-enchants/docs-reorder'
+
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
   return doc?.title ? `${doc.title} | ${WEBSITE_NAME}` : WEBSITE_NAME
 }
@@ -31,6 +37,13 @@ const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
 }
 
 export const plugins: Plugin[] = [
+  // Custom plugins
+  betterLocalizedFields(),
+  docsReorder({
+    collections: ['pages'].map((slug) => ({ slug })),
+  }),
+
+  // Default plugins
   redirectsPlugin({
     collections: ['pages', 'posts'],
     overrides: {
